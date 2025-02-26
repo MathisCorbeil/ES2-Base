5ffc390be2b36d8edb49ae4bcf0df21ec1b7716d
MathisCorbeil <catmath23@gmail.com>
2025-02-26 11:21:27 -0500

    animeation preque fini

--- Assets/Scripts/ControleSousmarin.cs
+++ Assets/Scripts/ControleSousmarin.cs
@@ -8,23 +8,51 @@ public class ControleSousmarin : MonoBehaviour
     [SerializeField] private float _vitessePromenade;
     private Rigidbody _rb;
     private Vector3 directionInput;
+    private Animator _animator;
     // Start is called before the first frame update
+
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     
+void OnBoost(InputValue etatBouton){
+    if(etatBouton.isPressed){
+    Debug.Log("patate");
+          _vitessePromenade=_vitessePromenade * 2;
+    }
+      else
+        {
+            Debug.Log("patate_piler");
+            _vitessePromenade = _vitessePromenade/2;
+           
 
+        }
+      }
 
     void OnAvant(InputValue directionBase)
     {
         float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
         directionInput = new Vector3(0f, 0f, directionAvecVitesse);
-        
+        _animator.SetFloat("Deplacement_Avant", directionInput.magnitude);
+
 
     }
+
+    void OnVerticale(InputValue directionBase){
+          float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
+        directionInput = new Vector3(0f, directionAvecVitesse, 0f);
+        _animator.SetFloat("Deplacement_Verticale", directionInput.magnitude);
+
+    }
+
+    
+    
+    
 
 void FixedUpdate()
     {
