a34790887bb34edbcce3027fbe7856827fb5e9f4
MathisCorbeil <catmath23@gmail.com>
2025-02-26 12:22:27 -0500

    pas fini

--- Assets/Scripts/ControleSousmarin.cs
+++ Assets/Scripts/ControleSousmarin.cs
@@ -20,11 +20,49 @@ public class ControleSousmarin : MonoBehaviour
 
     // Update is called once per frame
     
-void OnBoost(InputValue etatBouton){
+
+
+    void OnAvant(InputValue directionBase)
+    {
+       
+
+        float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
+        directionInput = new Vector3(0f, 0f, directionAvecVitesse);
+       
+
+        _animator.SetFloat("Deplacement_Avant", directionInput.magnitude);
+        
+
+
+    }
+
+    void OnVerticale(InputValue directionBase){
+        
+
+          float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
+        
+        directionInput = new Vector3(0f, directionAvecVitesse, 0f);
+      
+        if(directionAvecVitesse < 0f)
+        {
+            _animator.SetBool("revers", true);
+
+        }
+        
+            _animator.SetFloat("Deplacement_Verticale", directionInput.magnitude);
+       
+    }
+
+    
+    
+    void OnBoost(InputValue etatBouton){
     if(etatBouton.isPressed){
     Debug.Log("patate");
           _vitessePromenade=_vitessePromenade * 2;
-    }
+          
+
+            
+        }
       else
         {
             Debug.Log("patate_piler");
@@ -33,26 +71,6 @@ void OnBoost(InputValue etatBouton){
 
         }
       }
-
-    void OnAvant(InputValue directionBase)
-    {
-        float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
-        directionInput = new Vector3(0f, 0f, directionAvecVitesse);
-        _animator.SetFloat("Deplacement_Avant", directionInput.magnitude);
-
-
-    }
-
-    void OnVerticale(InputValue directionBase){
-          float directionAvecVitesse = directionBase.Get<float>() * _vitessePromenade;
-        directionInput = new Vector3(0f, directionAvecVitesse, 0f);
-        _animator.SetFloat("Deplacement_Verticale", directionInput.magnitude);
-
-    }
-
-    
-    
-    
 
 void FixedUpdate()
     {
